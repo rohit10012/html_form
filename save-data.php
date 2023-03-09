33926a748f359e72d54079982c8c58f2dd732612
Rohit Sharma <rohit10012@gmail.com>
2023-03-09 19:46:53 +0530

    Add files via upload

---
+++ save-data.php
@@ -0,0 +1,36 @@
+<?php
+// Define MySQL database connection variables
+$servername = "localhost";
+$username = "root";
+$password = "E5nmRBG7@";
+$dbname = "user_data";
+
+// Create a new MySQL database connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check if the connection is successful
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Get the form data from the $_POST superglobal array
+$name = $_POST["name"];
+$email = $_POST["email"];
+$mobile = $_POST["mobile"];
+
+// Prepare a SQL statement to insert the data into the database
+$stmt = $conn->prepare("INSERT INTO users (name, email, mobile) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $name, $email, $mobile);
+
+// statement execution is successful
+if ($stmt->execute()) {
+echo "Data saved successfully!";
+} else {
+echo "Error: " . $stmt->error;
+}
+
+// Close the database connection and statement
+$stmt->close();
+$conn->close();
+?>
+
