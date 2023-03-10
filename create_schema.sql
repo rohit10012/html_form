50b252ceb0685b5acb70c19495c46b8eb9884956
Rohit Sharma <rohit10012@gmail.com>
2023-03-10 14:49:12 +0530

    Create create_schema.sql

---
+++ create_schema.sql
@@ -0,0 +1,2 @@
+create database user_data;
+CREATE TABLE users (   id INT(11) NOT NULL AUTO_INCREMENT,   name VARCHAR(50) NOT NULL,   email VARCHAR(100) NOT NULL,   mobile VARCHAR(20) NOT NULL,   PRIMARY KEY (id) );
